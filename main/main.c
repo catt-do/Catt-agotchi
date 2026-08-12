3cf7cfde9180ebf36170546caac1af3919e61151
91tleg <maxchou0417@gmail.com>
2026-08-12 16:31:09 -0700

    Add SPI Display Pin Configuration
    closes #16

--- main/main.c
+++ main/main.c
@@ -2,14 +2,29 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "buttons.h"
+#include "display.h"
 
 #define TAG "Main"
 
-static button_t left = { .pin = GPIO_NUM_4 };
+static button_t left   = { .pin = GPIO_NUM_4 };
+static button_t right  = { .pin = GPIO_NUM_5 };
+static button_t power  = { .pin = GPIO_NUM_6 };
+static button_t select = { .pin = GPIO_NUM_7 };
+
+static display_pins_t display_pins = {
+    .mosi  = GPIO_NUM_11,
+    .sclk  = GPIO_NUM_12,
+    .cs    = GPIO_NUM_10,
+    .dc    = GPIO_NUM_9,
+    .reset = GPIO_NUM_8,
+    .bl    = GPIO_NUM_13
+};
 
 void app_main(void)
 {
     button_init(&left);
+    display_init(&display_pins);
+
     for(;;) {
         if(button_is_pressed(&left)) {
             ESP_LOGI(TAG, "Button pressed");
